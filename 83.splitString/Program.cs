b7905a978c2ca3eb647d7e51762d55b49a4b4b10
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-06 23:16:50 +0530

    split string is worlds

---
+++ 83.splitString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System ;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+       static void Main()
+    {
+        String str = "my name is shree";
+
+        string [] worlds = str.Split(" ");
+
+        foreach(string world in worlds)
+        {
+            Console.WriteLine(world);
+        }
+    }
+}
