3e0a1edc5d75b580fea0b62b72f5baff6dd0e942
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-06 23:50:09 +0530

    remove special charecher

---
+++ 85.removeSpecialChar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "shree@gmail.com";
+        string result = "";
+
+        foreach(char ch in str1)
+        {
+            if (char.IsLetterOrDigit(ch))
+            {
+                result += ch;
+            }
+        }
+
+        Console.WriteLine("Original String:"+ str1);
+
+        Console.WriteLine("After removing special Characters: "+result);
+
+    }
+}
