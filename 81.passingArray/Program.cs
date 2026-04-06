cbeabf1164cee0b2632de732669ce2bfa9aa6971
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-06 22:51:29 +0530

    passing array in method

---
+++ 81.passingArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System ;
+
+class Program
+{
+    static void DispayArray(int [] arr)
+    {
+      Console.WriteLine("Array elements are: ");  
+
+      foreach(int num in arr)
+        {
+            Console.Write(num +" ");
+        }
+    }
+    static void Main()
+    {
+        int [] numbers = {3,13,32,29,20,21,73};
+
+        DispayArray(numbers);
+    }
+}
