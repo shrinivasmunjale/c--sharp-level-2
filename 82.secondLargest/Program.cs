39c590a2eef0344a755ddf4c1f37bea6121a6357
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-06 23:06:41 +0530

    second largest number

---
+++ 82.secondLargest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System ;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void DispayArray(int [] arr)
+    {
+      int largest = int.MinValue;
+      int second = int.MinValue;
+
+      foreach(int num in arr)
+        {
+            if(num > largest)
+            {
+                second = largest;
+                largest = num;
+            }
+            else if (num > second && num != largest)
+            {
+                second = num;
+            }
+        }
+             if (second == int.MinValue)
+            Console.WriteLine("No second largest element found.");
+        else
+            Console.WriteLine("Second Largest Element: " + second);
+    }
+    static void Main()
+    {
+        int [] numbers = {3,13,32,29,20,21,73};
+
+        DispayArray(numbers);
+    }
+}
