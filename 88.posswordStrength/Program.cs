c2736a099b5e9e0b97cd0ad426fa479d991aa5c7
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-08 23:22:58 +0530

    password strength

---
+++ 88.posswordStrength/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+class Program
+{
+
+    static bool isStrongPassword(string pass)
+    {
+        bool hasupper = false;
+        bool haslower = false;
+        bool hasdigit = false;
+        bool hasspecial = false;
+
+        if(pass.Length < 8)
+        {
+            return false;
+        }
+
+        foreach(char ch in pass)
+        {
+            if (char.IsUpper(ch))
+            {
+                hasupper = true;
+            }
+            else if (Char.IsDigit(ch))
+            {
+                hasdigit = true;
+            }
+             else if (Char.IsLower(ch))
+            {
+                haslower = true;
+            }
+            else
+            {
+                hasspecial = true;
+            }
+        }
+            return hasupper && haslower && hasdigit && hasspecial;
+    }
+    static void Main()
+    {
+        Console.Write("Enter the Password : ");
+        string password = Console.ReadLine();
+
+        if (isStrongPassword(password))
+        {
+            Console.WriteLine("password is strong");
+        }
+        else
+        {
+            Console.WriteLine("Password is week..");
+        }
+    }
+}
