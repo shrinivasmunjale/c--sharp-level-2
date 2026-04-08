32ea60347e6354471f25e23a11a7eb5161d41476
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-08 22:02:38 +0530

    convert upper or lover case

---
+++ 86.convertString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string st = "hello from C#";
+
+        string upper = st.ToUpper();
+
+        string lower = st.ToLower();
+
+        Console.WriteLine("After uppercase : "+ upper);
+        Console.WriteLine("After lover case : "+ lower);
+    }
+}
