e44a2a4d3536405b01da978147baa3c6295312cc
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-08 22:17:23 +0530

    validate email

---
+++ 87.validateEmail/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the Email : ");
+        string email = Console.ReadLine();
+
+        if(email.Contains("@") && email.Contains("."))
+        {
+            Console.WriteLine("Valid Email");
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid Email");
+        }
+    }
+}
