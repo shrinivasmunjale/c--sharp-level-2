bcfcce6f4ed18180d1ea8e16590a2936bacc6eb0
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-15 01:25:47 +0530

    find missing number

---
+++ 95.missingNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int [] arr = {1,2,4,5};
+        int n = 5;
+
+        int sum = n * (n+1)/2;
+
+        int actualsum = 0;
+
+        for(int i = 0; i < arr.Length ; i++)
+        {
+            actualsum += arr[i]; 
+        }
+
+        int missnum = sum - actualsum;
+
+        Console.WriteLine("missing number is: "+missnum);
+    }
+}
