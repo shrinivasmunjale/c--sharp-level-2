084babf7094939c57add1e732605f25d42aec5b3
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-16 00:22:54 +0530

    level 2 programs

---
+++ memory management/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Demo
+{
+    public int value;
+
+    public Demo(int v)
+    {
+        value = v;
+        Console.WriteLine("Object created with value: " + value);
+    }
+
+    ~Demo() // Destructor (called by Garbage Collector)
+    {
+        Console.WriteLine("Object destroyed (Garbage Collected)");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // 🔸 VALUE TYPE (Stored in Stack)
+        int a = 10;
+        Console.WriteLine("Value type (Stack): " + a);
+
+        // 🔸 REFERENCE TYPE (Stored in Heap)
+        Demo obj1 = new Demo(100);
+        Demo obj2 = new Demo(200);
+
+        // Remove reference
+        obj1 = null;
+
+        Console.WriteLine("Forcing Garbage Collection...");
+
+        // Force Garbage Collection
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("End of Program");
+    }
+}
