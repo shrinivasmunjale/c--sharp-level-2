8dad0089f1b105adf862990f4c8c8fa4837a38a2
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-15 23:16:00 +0530

    dupliate elements

---
+++ 96.duplicateEle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int [] array ={2,4,3,4,5,5};
+
+        Console.WriteLine("Duplicate elements:");
+
+        for (int i = 0 ; i < array.Length; i++)
+        {
+            for ( int j = i+1 ; j < array.Length ; j++)
+            {
+                if ( array[i] == array[j])
+                {
+                    Console.Write(array[j]+" ");
+                    break;
+                }
+            }
+        }
+    }
+}
