1d427ca6a4dc39c89863fcedd2cba1c94a5df812
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-15 23:51:45 +0530

    span and ReadOnlySpan

---
+++ 97.span/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int [] array = {34,2,45,3,22};
+
+        Span<int> span = array;
+
+        Console.Write ("Original Array: ");
+        foreach(int i in array)
+        {
+            Console.Write(i +" ");
+        }
+
+
+        span[1] = 99;
+        span[2] = 32;
+
+        Console.WriteLine("\n\n Modified Array using Span:");
+
+        foreach( int n in array)
+        {
+            Console.Write(n +" ");
+        }
+
+         // Create ReadOnlySpan
+        ReadOnlySpan<int> readOnlySpan = Array;
+
+          Console.WriteLine("ReadOnlySpan Elements:");
+
+        foreach (var n in readOnlySpan)
+            Console.Write(n + " ");
+
+          //  This will give compile-time error:
+        // readOnlySpan[1] = 100;   
+    }
+}
